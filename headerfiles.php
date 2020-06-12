16e0eb0d95e3933d8411b684e9a1484e4b7db2ee
Chand-Madaan <64115038+Chand-Madaan@users.noreply.github.com>
2020-06-12 11:59:16 +0930

    Add files via upload

---
+++ headerfiles.php
@@ -0,0 +1,17 @@
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="jquery-ui.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script> <!-- Necessary-JavaScript-File-For-Bootstrap -->
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/jquery.validate.js"></script>
+<script type="text/javascript" src="js/myscript.js"></script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" /> <!-- Style-CSS -->
+<link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" /> <!-- Banner-Slider-CSS -->
+<!-- //css files -->
+<link rel="stylesheet" type="text/css" href="css/demo.css"/>
+
+
